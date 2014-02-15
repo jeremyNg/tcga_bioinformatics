4e35d0f775a0b663c55eef13103fdd68311715ac
Ng Wee Kiat Jeremy <jeremy.ng.wk1990@gmail.com>
2014-02-16 01:32:00 +0800

    refresh of bash scripts

--- bash.sh
+++ bash.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # CHUNK 1: In line edit for gene normalized results
-sed -i -e '/?/d' *rsem.genes.normalized_results |sed -i -e 's/|[ 0-9 ]//g' *rsem.genes.normalized_results
+sed -i '' '/?/d' *rsem.genes.normalized_results |sed -i '' 's/|[ 0-9 ]*//g' *rsem.genes.normalized_results
